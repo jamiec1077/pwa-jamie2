278aba86a5c13899862776527425cc72efd3236a
jamiec1077 <jamie.charlemagne1992@gmail.com>
2023-06-08 11:06:46 -0400

    fixed the database.jsfile

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+console.error('putDb not implemented');
+const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('the database is safe with me', result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+ console.error('getDb not implemented');
+const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+console.log('data has been found!!', result,value)
+console.log('database cant find it sorry');
+return result?.value;
+};
 
 initdb();
